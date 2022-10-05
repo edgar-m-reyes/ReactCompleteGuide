6dab1807d7e8173398f04d4e67e18d5a0e66e1a1
Edgar Morales Reyes <edgarmoralesreyes@Mac-mini.local>
2022-10-04 19:42:38 -0700

    React Basics multiple components, properties, children

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,21 @@
+import Card from '../UI/Card';
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
+
+function Expenses(props) {
+    return (
+        <Card className='expenses'>
+            {props.expenses.map((expense) => {
+                return (
+                    <ExpenseItem
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    ></ExpenseItem>
+                )
+            })}
+        </Card>
+    )
+}
+
+export default Expenses;
